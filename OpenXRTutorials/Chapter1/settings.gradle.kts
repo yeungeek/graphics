947a7296879ff9e11c2882119a5fd6371ae37b03
yangjian <jian.yang@rokid.com>
2024-03-01 17:00:04 +0800

    [add] add openxr intro chapter

---
+++ OpenXRTutorials/Chapter1/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ch1-Intro"
+include(":app")
+ 
