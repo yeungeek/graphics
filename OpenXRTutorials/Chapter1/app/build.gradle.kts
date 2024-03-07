5280e70869122497b88248aa2a8d3a3f5f607967
yangjian <jian.yang@rokid.com>
2024-03-07 14:45:56 +0800

    [update] update ndk abi

--- OpenXRTutorials/Chapter1/app/build.gradle.kts
+++ OpenXRTutorials/Chapter1/app/build.gradle.kts
@@ -15,8 +15,11 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
 
+        ndk {
+            abiFilters.add("arm64-v8a")
+        }
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -47,11 +50,4 @@ android {
 dependencies {
 
     implementation("org.khronos.openxr:openxr_loader_for_android:1.0.28.2")
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
