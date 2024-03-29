ce1a6a6bc7d2a959dd8f60cdc3de367e6bd0e538
yeungeek <yangjian0410@gmail.com>
2024-03-29 20:25:45 +0800

    [update] update extern android_main method

---
+++ OpenXRTutorials/HelloXR/app/src/main/cpp/intro.cpp
@@ -0,0 +1,16 @@
+//
+// Created by jian.yang on 2024/3/29.
+//
+#include "common/logger.h"
+#include "common/common.h"
+
+#include <android/log.h>
+#include <android_native_app_glue.h>
+#include <android/native_window.h>
+#include <jni.h>
+#include <sys/system_properties.h>
+
+extern "C"
+void android_main(struct android_app *app) {
+
+}
