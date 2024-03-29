be14acab52c19afcde7a4f3beca700b7e664f97a
yangjian <jian.yang@rokid.com>
2024-03-29 16:44:22 +0800

    [update] add platform and openxr program

--- OpenXRTutorials/HelloXR/app/src/main/cpp/main.cpp
+++ OpenXRTutorials/HelloXR/app/src/main/cpp/main.cpp
@@ -3,6 +3,10 @@
 #include "common/logger.h"
 #include "common/common.h"
 #include "common/options.h"
+#include "common/platformdata.h"
+#include "common/platformplugin.h"
+#include "common/graphicsplugin.h"
+#include "common/openxr_program.h"
 
 bool UpdateOptionsFromSystemProperties(Options& options) {
 #if defined(DEFAULT_GRAPHICS_PLUGIN_OPENGLES)
@@ -46,7 +50,53 @@ struct AndroidAppState {
  * Process the next main command.
  */
 static void app_handle_cmd(struct android_app* app, int32_t cmd) {
+    AndroidAppState* appState = (AndroidAppState*)app->userData;
 
+    switch (cmd) {
+        // There is no APP_CMD_CREATE. The ANativeActivity creates the
+        // application thread from onCreate(). The application thread
+        // then calls android_main().
+        case APP_CMD_START: {
+            Log::Write(Log::Level::Info, "    APP_CMD_START");
+            Log::Write(Log::Level::Info, "onStart()");
+            break;
+        }
+        case APP_CMD_RESUME: {
+            Log::Write(Log::Level::Info, "onResume()");
+            Log::Write(Log::Level::Info, "    APP_CMD_RESUME");
+            appState->Resumed = true;
+            break;
+        }
+        case APP_CMD_PAUSE: {
+            Log::Write(Log::Level::Info, "onPause()");
+            Log::Write(Log::Level::Info, "    APP_CMD_PAUSE");
+            appState->Resumed = false;
+            break;
+        }
+        case APP_CMD_STOP: {
+            Log::Write(Log::Level::Info, "onStop()");
+            Log::Write(Log::Level::Info, "    APP_CMD_STOP");
+            break;
+        }
+        case APP_CMD_DESTROY: {
+            Log::Write(Log::Level::Info, "onDestroy()");
+            Log::Write(Log::Level::Info, "    APP_CMD_DESTROY");
+            appState->NativeWindow = NULL;
+            break;
+        }
+        case APP_CMD_INIT_WINDOW: {
+            Log::Write(Log::Level::Info, "surfaceCreated()");
+            Log::Write(Log::Level::Info, "    APP_CMD_INIT_WINDOW");
+            appState->NativeWindow = app->window;
+            break;
+        }
+        case APP_CMD_TERM_WINDOW: {
+            Log::Write(Log::Level::Info, "surfaceDestroyed()");
+            Log::Write(Log::Level::Info, "    APP_CMD_TERM_WINDOW");
+            appState->NativeWindow = NULL;
+            break;
+        }
+    }
 }
 
 /**
@@ -68,6 +118,78 @@ void android_main(struct android_app *app) {
         return;
     }
 
+    std::shared_ptr<PlatformData> data = std::make_shared<PlatformData>();
+    data->applicationVM = app->activity->vm;
+    data->applicationActivity = app->activity->clazz;
+
+    std::shared_ptr<IPlatformPlugin> platformPlugin = CreatePlatformPlugin(options, data);
+    Log::Write(Log::Level::Info,"###### 1.CreatePlatformPlugin");
+    // Create graphics API implementation.
+    std::shared_ptr<IGraphicsPlugin> graphicsPlugin = CreateGraphicsPlugin(options, platformPlugin);
+    Log::Write(Log::Level::Info,"###### 2.CreateGraphicsPlugin");
+    // Initialize the OpenXR program.
+    std::shared_ptr<IOpenXrProgram> program = CreateOpenXrProgram(options, platformPlugin, graphicsPlugin);
+    Log::Write(Log::Level::Info,"###### 3.CreateOpenXrProgram");
+
     // Initialize the loader for this platform
     PFN_xrInitializeLoaderKHR initializeLoader = nullptr;
+    if (XR_SUCCEEDED(
+            xrGetInstanceProcAddr(XR_NULL_HANDLE, "xrInitializeLoaderKHR", (PFN_xrVoidFunction*)(&initializeLoader)))) {
+        XrLoaderInitInfoAndroidKHR loaderInitInfoAndroid = {XR_TYPE_LOADER_INIT_INFO_ANDROID_KHR};
+        loaderInitInfoAndroid.applicationVM = app->activity->vm;
+        loaderInitInfoAndroid.applicationContext = app->activity->clazz;
+        initializeLoader((const XrLoaderInitInfoBaseHeaderKHR*)&loaderInitInfoAndroid);
+    }
+
+    program->CreateInstance();
+    program->InitializeSystem();
+
+    options->SetEnvironmentBlendMode(program->GetPreferredBlendMode());
+    UpdateOptionsFromSystemProperties(*options);
+    platformPlugin->UpdateOptions(options);
+    graphicsPlugin->UpdateOptions(options);
+
+    program->InitializeDevice();
+    program->InitializeSession();
+    program->CreateSwapchains();
+
+    bool requestRestart = false;
+    bool exitRenderLoop = false;
+
+    while (app->destroyRequested == 0) {
+        // Read all pending events.
+        for (;;) {
+            int events;
+            struct android_poll_source* source;
+            // If the timeout is zero, returns immediately without blocking.
+            // If the timeout is negative, waits indefinitely until an event appears.
+            const int timeoutMilliseconds =
+                    (!appState.Resumed && !program->IsSessionRunning() && app->destroyRequested == 0) ? -1 : 0;
+            if (ALooper_pollAll(timeoutMilliseconds, nullptr, &events, (void**)&source) < 0) {
+                break;
+            }
+
+            // Process this event.
+            if (source != nullptr) {
+                source->process(app, source);
+            }
+        }
+
+        program->PollEvents(&exitRenderLoop, &requestRestart);
+        if (exitRenderLoop) {
+            ANativeActivity_finish(app->activity);
+            continue;
+        }
+
+        if (!program->IsSessionRunning()) {
+            // Throttle loop since xrWaitFrame won't be called.
+            std::this_thread::sleep_for(std::chrono::milliseconds(250));
+            continue;
+        }
+
+        program->PollActions();
+        program->RenderFrame();
+    }
+
+    app->activity->vm->DetachCurrentThread();
 }
